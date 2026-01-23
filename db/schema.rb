74c82320c9f3c80ae704adc5fd5f2c91366c1d83
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-01-23 14:01:59 -0500

    Created matches

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_01_23_184857) do
+  create_table "matches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "sock_1_id"
+    t.integer "sock_2_id"
+    t.datetime "updated_at", null: false
+    t.index ["sock_1_id"], name: "index_matches_on_sock_1_id"
+    t.index ["sock_2_id"], name: "index_matches_on_sock_2_id"
+  end
+
+  create_table "socks", force: :cascade do |t|
+    t.integer "age"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "gender"
+    t.string "height"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "matches", "socks", column: "sock_1_id"
+  add_foreign_key "matches", "socks", column: "sock_2_id"
+end
