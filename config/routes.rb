d7249a7bf1a30d3811e1a910db3fa1e8b2aaae53
Angel Hernandez Barcenas <angelhb015@gmail.com>
2026-01-23 14:21:31 -0500

    created matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # does this file contain the routes that the user can navigate to?
 Rails.application.routes.draw do
+  resources :matches
   resources :socks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
